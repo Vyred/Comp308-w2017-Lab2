b1cf9091a18c16b9e4a810599bc2a8c0efb57422
vyred <tismatthew@hotmail.com>
2017-03-11 14:43:15 -0500

    seventh commit

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 module.exports = {
   //local MongoDB deployment ->
   //"URI": "mongodb://localhost/contactsDB"
-  "URI": process.env.URI
-
+  //"URI": process.env.URI
    //mongoose.connect(process.env.URI || config.URI);
+   "URI" : "mongodb://<dbuser>:<dbpassword>@ds129050.mlab.com:29050/lab2database"
 };
