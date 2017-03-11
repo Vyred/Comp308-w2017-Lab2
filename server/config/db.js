88f6ee5454804472189b0ca69847ae1ea15f6851
vyred <tismatthew@hotmail.com>
2017-03-11 13:49:58 -0500

    fourth commit: variable name changes and uri edit

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
-module.exports = {
+//module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/contactsDB"
-  //Remote MongoDB deployment -> let URI = "mongodb://thomas:123456@ds054999.mlab.com:54999/videogames";
-};
+  //"URI": "mongodb://localhost/contactsDB"
+   mongoose.connect(process.env.URI || config.URI);
+//};
