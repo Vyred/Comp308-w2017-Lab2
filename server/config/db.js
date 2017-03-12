63f4aff20dc98555628f7741f7fa8af7c1b4016d
vyred <tismatthew@hotmail.com>
2017-03-12 07:49:37 -0400

    eighth commit - uri change

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
   //"URI": "mongodb://localhost/contactsDB"
-  //"URI": process.env.URI
-   //mongoose.connect(process.env.URI || config.URI);
-   "URI" : "mongodb://<dbuser>:<dbpassword>@ds129050.mlab.com:29050/lab2database"
-};
+  "URI" : "mongodb:Vired:greatgreygod1@ds129050.mlab.com:29050/lab2database"
+}
